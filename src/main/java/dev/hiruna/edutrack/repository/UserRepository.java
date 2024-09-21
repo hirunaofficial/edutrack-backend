f32cd3b4e25bbbb7079ddb8e28500ad6e64bc06a
GD Hiruna <hirunaofficial@gmail.com>
2024-09-21 18:53:31 +0530

    Update UserRepository to include findByEmail method for login functionality

--- src/main/java/dev/hiruna/edutrack/repository/UserRepository.java
+++ src/main/java/dev/hiruna/edutrack/repository/UserRepository.java
@@ -2,7 +2,9 @@ package dev.hiruna.edutrack.repository;
 
 import dev.hiruna.edutrack.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
-
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
 }
